0cab182577ada1b69855ed18b8931dfe1834bbf5
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2022-12-30 11:41:21 -0500

    AST parser

--- jlox/build.gradle.kts
+++ jlox/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.charlesbourget.lox"
 version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
+
+application {
+    mainClass.set("com.charlesbourget.lox.Lox")
+}
--- jlox/src/main/java/com/charlesbourget/lox/Lox.java
+++ jlox/src/main/java/com/charlesbourget/lox/Lox.java
@@ -55,9 +55,15 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) {
+            return;
         }
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -67,5 +73,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.printf("[line %d] Error %s: %s%n", line, where, message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, String.format(" at '%s'", token.lexeme), message);
+        }
     }
 }
