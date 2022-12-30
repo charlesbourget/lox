11a05bd8e0693cdcbb2666c8063d4b62ccc46ef7
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2022-12-30 09:42:38 -0500

    Scanner and AST parser

---
+++ jlox/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("java")
+}
+
+group = "com.charlesbourget.lox"
+version = "1.0-SNAPSHOT"
---
+++ jlox/src/main/java/com/charlesbourget/lox/tool/GenerateAst.java
@@ -0,0 +1,74 @@
+package com.charlesbourget.lox.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression", "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = String.format("%s/%s.java", outputDir, baseName);
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
+
+        writer.println("package com.charlesbourget.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.printf("public interface %s {%n", baseName);
+
+        defineVisitor(writer, baseName, types);
+        writer.println();
+
+        // The AST classes.
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+            writer.println();
+        }
+
+        // The base accept() method.
+        writer.println("  <R> R accept(Visitor<R> visitor);");
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.printf("  record %s(%s) implements %s {%n", className, fieldList, baseName);
+
+        // Visitor pattern.
+        writer.println("    @Override");
+        writer.println("    public <R> R accept(Visitor<R> visitor) {");
+        writer.printf("      return visitor.visit%s%s(this);%n", className, baseName);
+        writer.println("    }");
+
+        writer.println("  }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.printf("    R visit%s%s(%s %s);%n", typeName, baseName, typeName, baseName.toLowerCase());
+        }
+
+        writer.println("  }");
+    }
+
+}
