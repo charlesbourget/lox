945ba0e3c9d2629d086036d43db0d18baee42a25
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2023-01-02 18:27:55 -0500

    initial clox project files

---
+++ clox/common.h
@@ -0,0 +1,12 @@
+//
+// Created by Charles Bourget on 2023-01-02.
+//
+
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //CLOX_COMMON_H
